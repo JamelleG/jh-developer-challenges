f530b1ce94a49e456ff6a150474dccb7557c8336
jamelleg <jamelleg@outlook.com>
2017-11-08 14:49:20 +0000

    Completed both challenges

--- src/ShippingDates.php
+++ src/ShippingDates.php
@@ -1,11 +1,72 @@
 <?php
 
 namespace Jh\Shipping;
+
 /**
  * Class ShippingDates
  * @package Jh\Shipping
  */
 class ShippingDates implements ShippingDatesInterface
 {
+    const SHIP_DAYS = 3;
+    const DAYS_IN_WEEK = 7;
+    const WEEKEND_START = 6;
+    
+    
+    /**
+     * Calculates delivery date using a order date.
+     * 
+     * @param \DateTime $orderDate
+     * @return \DateTime
+     */
+    public function calculateDeliveryDate(\DateTime $orderDate)
+    {
+        
+        $dispatchDate = self::calculateDispatchDate($orderDate);
+        $deliveryDate = clone $dispatchDate;
+        
+        for ($businessDays = 0; $businessDays < self::SHIP_DAYS;) {
+            
+            $deliveryDate->add(new \DateInterval('P1D'));
+            
+            if (self::isValidDate($deliveryDate)) {
+                $businessDays++;
+            }
+            
+        }
 
+        return $deliveryDate;
+    }
+
+    /**
+     * Calculates dispatch date using order date.
+     *
+     * @param \DateTime $orderDate
+     * @return \DateTime
+     */
+    private function calculateDispatchDate(\DateTime $orderDate) : \DateTime
+    {
+        $date = clone $orderDate;
+       
+        if ($date->format('N') >= self::WEEKEND_START) {
+            $daysTillNextProcess = self::DAYS_IN_WEEK - $date->format('N') + 1;
+
+            $date->add(new \DateInterval('P'. $daysTillNextProcess .'D'));
+        }
+
+        return $date;
+    }
+    /**
+     * Checks if a date is valid using pre-defined rules
+     * 
+     * @param \DateTime $date
+     * @return boolean
+     */
+    private function isValidDate(\DateTime $date)
+    {
+        if ($date->format('N') >= self::WEEKEND_START) {
+                return false;
+        }
+        return true;
+    }
 }
